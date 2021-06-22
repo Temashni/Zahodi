4746ea80f72288a885ff3ae7bdb1ffa6ed6e8579
Artem Mishin <artem.mishinbet98@mail.ru>
2021-06-22 10:13:10 +0400

    add drop-button-logo

--- script.js
+++ script.js
@@ -22,4 +22,33 @@ const autoswiper = new Swiper('.Swiper', {
     },
   });
 
-  function displ(ddd) { if (document.getElementById(ddd).style.display == 'none') {document.getElementById(ddd).style.display = 'block'} else {document.getElementById(ddd).style.display = 'none'} }
+var isShowAdditionLogos = false;
+
+
+function toggleShowAdditionlogos () {
+  isShowAdditionLogos = !isShowAdditionLogos;
+  var logoWrapperRef = document.getElementById("logoWrapper");
+  var logoWrapperBtnRef = document.getElementById("logoWrapperBtn");
+  if (isShowAdditionLogos) {
+    logoWrapperRef.style.display = "block";
+    logoWrapperBtnRef.innerText = "Скрыть";
+  } else {
+    logoWrapperRef.style.display = "none";
+    logoWrapperBtnRef.innerText = "Показать ещё";
+  }
+}
+
+var isShowLogos = false;
+
+function toggleShowlogos() {
+  isShowLogos = !isShowLogos;
+  var logoContainerRef = document.getElementById("logoContainer");
+  var logoContainerBtnRef = document.getElementById("logoContainerBtn");
+  if (isShowLogos) {
+    logoContainerRef.style.display = "block";
+    logoContainerBtnRef.innerText = "Скрыть";
+  } else {
+    logoContainerRef.style.display = "none";
+    logoContainerBtnRef.innerText = "Показать ещё";
+  }
+}
